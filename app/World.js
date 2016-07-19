f3dfbf7d21f72480694438d243e3c0ba807f6f9e
ansanders <ansanders@uni-osnabrueck.de>
2016-07-19 11:50:09 +0200

    add starparticles

--- app/World.js
+++ app/World.js
@@ -1,4 +1,35 @@
+var stars = []; 
 
+function updateStars(){
+
+
+}
+
+
+function createStars(){
+
+    var star, material; 
+
+    for ( var zpos= -1000; zpos < 1000; zpos+=20 ) {
+
+        //Material of stars
+        material = new THREE.ParticleCanvasMaterial( { color: 0xffffff, program: particleRender } );
+        // make the star
+        star = new THREE.Particle(material);
+        
+        // give it a random x and y position between -500 and 500
+        particle.position.x = Math.random() * 1000 - 500;
+        particle.position.y = Math.random() * 1000 - 500;
+
+        star.position.z = zpos; 
+        star.scale.x = star.scale.y = 10;
+
+        scene.add(star); 
+
+        stars.push(star); 
+    }
+
+}
 function World(){
 
     return {
